fce08e15fe285cb22ca5fd600755278ce45bad21
James Loosli <james.loosli@hart.com>
2016-11-17 13:43:42 -0800

    updated deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This deploys code to a saltmaster, running locally on macOS
+# It does not restart the salt-master daemon
+
 # Be in the right place.
 DIR=$(dirname $0)
 cd $DIR/..
@@ -9,19 +12,21 @@ git pull -q
 echo "Deploying `git log | head -1`"
 
 # Loop through files, rsync aggressively, but ignore certain files
-rsync -avd \
+echo "Syncing Files"
+sudo rsync -aid \
   --exclude '/etc/salt/keys/*' \
   --exclude '/etc/salt/pki/*' \
-  ./etc/salt /etc/ 
+  ./etc/salt /etc/
 
-rsync -avd \
+sudo rsync -aid \
   ./srv / 
 
 # Populate the aws keys
-AWS_ID=$(grep "aws_access_key_id" ~/.aws/credentials | head -1)
-AWS_KEY=$(grep "aws_secret_access_key" ~/.aws/credentials | head -1)
+echo "Populating Keys"
+AWS_ID=$(grep "aws_access_key_id" ~/.aws/credentials | head -1 | awk '{ print $3 } ')
+AWS_KEY=$(grep "aws_secret_access_key" ~/.aws/credentials | head -1 | awk '{ print $3}')
 
-if [ AWS_ID != '' ] ; then
-  sed -i "s/@AWS_ACCESS_KEY_ID@/${AWS_ID}/" /etc/salt/cloud.providers.d/salt-demo.conf
-  sed -i "s/@AWS_SECRET_ACCESS_KEY@/${AWS_KEY}/" /etc/salt/cloud.providers.d/salt-demo.conf
+if [[ ! -z $AWS_ID ]] ; then
+  bash -c "sed -i '' 's^@AWS_ACCESS_KEY_ID@^${AWS_ID}^' /etc/salt/cloud.providers.d/salt-demo.conf"
+  bash -c "sed -i '' 's^@AWS_SECRET_ACCESS_KEY@^${AWS_KEY}^' /etc/salt/cloud.providers.d/salt-demo.conf"
 fi
